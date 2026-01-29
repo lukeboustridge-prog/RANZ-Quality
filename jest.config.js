7e5f273259d29bceda82b42895ddec72f46e9fbe
Luke Boustridge <luke.boustridge@gmail.com>
2026-01-29 22:37:43 +1300

    chore(08-04): install accessibility testing dependencies
    - Add jest-axe for automated WCAG accessibility testing
    - Add @testing-library/react and @testing-library/jest-dom
    - Configure Jest with jsdom environment for DOM testing
    - Add jest.setup.js with toHaveNoViolations extension
    - Add test and test:watch scripts to package.json

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: 'tsconfig.json',
+    }],
+  },
+  testMatch: ['**/__tests__/**/*.test.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
